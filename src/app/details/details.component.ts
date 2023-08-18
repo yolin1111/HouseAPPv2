91640fcdd0049a6f67295e0e6b252bcae9335f92
YolinChen <yolin1111@gmail.com>
2023-08-18 17:57:54 +0800

    v1.13

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+//import { Component } from '@angular/core'; //mark v1.13
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common'; //add v1.13
+import { ActivatedRoute } from '@angular/router'; //add v1.13
+import { HousingService } from '../housing.service'; //add v1.13
+import { HousingLocation } from '../housinglocation'; //add v1.13
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule
+  ],
+  /* mark v1.13
   template: `
     <p>
       details works!
     </p>
   `,
+  */
+  template: `<p>details works! {{ housingLocationId }}</p>`,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
-
+  //add v1.13
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = -1;
+  constructor() {
+      this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
 }
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,16 @@
 //import { Component } from '@angular/core';
-import { Component,Input } from '@angular/core';//mark v1.09
+import { Component, Input } from '@angular/core';//mark v1.09
 import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation'; //add v1.09
+import { RouterModule } from '@angular/router'; //add v1.13
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule //add v1.13
+  ],
   /* mark v1.09
   template: `
     <p>
@@ -14,13 +18,24 @@ import { HousingLocation } from '../housinglocation'; //add v1.09
     </p>
   `,
   */
+  /* mark v1.13
+    template: `
+    <section class="listing">
+      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    </section>
+    `,
+    */
   template: `
-  <section class="listing">
-    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
-    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
-  </section>
-  `,
+<section class="listing">
+  <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+  <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+  <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+  <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+</section>
+`,
+
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
