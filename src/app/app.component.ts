3c1992f7f36a8007aca25308a153f87dc5e13deb
YolinChen <yolin1111@gmail.com>
2023-08-18 14:14:57 +0800

    v1.03

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component'; //add v1.03
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `<h1>Hello World</h1>`,
+  imports: [
+    HomeComponent, //add v1.03
+  ],
+  //template: `<h1>Hello World</h1>`, //mark v1.03
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`, //add v1.03
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
