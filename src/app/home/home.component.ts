4d7f88538dcf0c4fd379192f712bd2ef218da903
YolinChen <yolin1111@gmail.com>
2023-08-18 14:23:43 +0800

    v1.04

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,21 @@ import { CommonModule } from '@angular/common';
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
+  /* mark v1.04
   template: `
     <p>
       home works!
     </p>
   `,
+  */
+  template: `
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+`,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
