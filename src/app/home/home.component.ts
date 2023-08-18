adb340cc122a21e715b4167776bbd46aeae998d1
YolinChen <yolin1111@gmail.com>
2023-08-18 16:39:10 +0800

    v1.09

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component'; //add v1.06
+import { HousingLocation } from '../housinglocation'; //add v1.09
 
 @Component({
   selector: 'app-home',
@@ -26,6 +27,7 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
   </section>
 `,
 */
+/* mark v1.09
 template: `
   <section>
     <form>
@@ -37,8 +39,32 @@ template: `
     <app-housing-location></app-housing-location>
   </section>
   `,
+  */
+  template: `
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+  <section class="results">
+  <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+  </section>
+  `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  //add v1.09
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+//import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';//mark v1.09
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation'; //add v1.09
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
+  /* mark v1.09
   template: `
     <p>
       housing-location works!
     </p>
   `,
+  */
+  template: `
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+  </section>
+  `,
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation; //add v1.09 驚嘆號功能為告訴TypeScript 編譯器該屬性的值不會為空或未定義
 }
