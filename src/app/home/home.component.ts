217195090f639bbd22be233dcf5cb3153d78d1eb
YolinChen <yolin1111@gmail.com>
2023-08-18 14:39:55 +0800

    v1.06

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component'; //add v1.06
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HousingLocationComponent //v1.06
+  ],
   /* mark v1.04
   template: `
     <p>
@@ -12,6 +16,7 @@ import { CommonModule } from '@angular/common';
     </p>
   `,
   */
+ /* mark v1.06
   template: `
   <section>
     <form>
@@ -20,6 +25,18 @@ import { CommonModule } from '@angular/common';
     </form>
   </section>
 `,
+*/
+template: `
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+  <section class="results">
+    <app-housing-location></app-housing-location>
+  </section>
+  `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
