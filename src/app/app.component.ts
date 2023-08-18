c9c6fd4f8ceb5769f0f3719711166cf0c5cf31bb
YolinChen <yolin1111@gmail.com>
2023-08-18 17:36:34 +0800

    v1.12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component'; //add v1.03
+import { RouterModule } from '@angular/router'; //add v1.12 導入路由文件
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HomeComponent, //add v1.03
+    RouterModule, //add v1.12 導入路由
   ],
   //template: `<h1>Hello World</h1>`, //mark v1.03
+  /* mark v1.12
   template: `
   <main>
     <header class="brand-name">
@@ -17,7 +20,21 @@ import { HomeComponent } from './home/home.component'; //add v1.03
       <app-home></app-home>
     </section>
   </main>
-`, //add v1.03
+`,
+ */
+
+template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@
 *  Protractor support is deprecated in Angular.
 *  Protractor is used in this example for compatibility with Angular documentation tools.
 */
-import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
+import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router'; //add v1.12 導入路由文件
+import routeConfig from './app/routes'; //add v1.12 導入路由文件
 
 bootstrapApplication(AppComponent,
-    {providers: [provideProtractorTestingSupport()]})
-  .catch(err => console.error(err));
+  {
+    providers: [
+      provideProtractorTestingSupport(),
+      provideRouter(routeConfig) //add v1.12 新增路由功能
+    ]
+  }
+).catch(err => console.error(err));
