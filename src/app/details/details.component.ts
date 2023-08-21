ab78033289dca6585d60fda6284e25eea1af9277
YolinChen <yolin1111@gmail.com>
2023-08-21 09:17:19 +0800

    v1.16

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common'; //add v1.13
 import { ActivatedRoute } from '@angular/router'; //add v1.13
 import { HousingService } from '../housing.service'; //add v1.13
 import { HousingLocation } from '../housinglocation'; //add v1.13
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'; //add v1.16
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule //add v1.16
   ],
   /* mark v1.13
   template: `
@@ -19,6 +21,8 @@ import { HousingLocation } from '../housinglocation'; //add v1.13
   `,
   */
   //template: `<p>details works! {{ housingLocationId }}</p>`, //mark v1.14
+
+  /* mark v1.16
   template: `
   <article>
     <img class="listing-photo" [src]="housingLocation?.photo"
@@ -37,6 +41,39 @@ import { HousingLocation } from '../housinglocation'; //add v1.13
     </section>
   </article>
 `,
+ */
+  template: `
+  <article>
+    <img class="listing-photo" [src]="housingLocation?.photo"
+      alt="Exterior photo of {{housingLocation?.name}}"/>
+    <section class="listing-description">
+      <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+      <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
+    </section>
+    <section class="listing-features">
+      <h2 class="section-heading">About this housing location</h2>
+      <ul>
+        <li>Units available: {{housingLocation?.availableUnits}}</li>
+        <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
+        <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
+      </ul>
+    </section>
+    <section class="listing-apply">
+      <h2 class="section-heading">Apply now to live here</h2>
+      <form [formGroup]="applyForm" (submit)="submitApplication()">
+        <label for="first-name">First Name</label>
+        <input id="first-name" type="text" formControlName="firstName">
+
+        <label for="last-name">Last Name</label>
+        <input id="last-name" type="text" formControlName="lastName">
+
+        <label for="email">Email</label>
+        <input id="email" type="email" formControlName="email">
+        <button type="submit" class="primary">Apply now</button>
+      </form>
+    </section>
+  </article>
+`,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
@@ -52,8 +89,24 @@ export class DetailsComponent {
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
 
+  //add v1.16
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  //add v1.16
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 }
