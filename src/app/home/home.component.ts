5fe47a36929d291956cbc2267c7967035dffe4ec
YolinChen <yolin1111@gmail.com>
2023-08-21 09:27:45 +0800

    v1.17

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -55,20 +55,33 @@ import { HousingService } from '../housing.service'; //add v1.11
   </section>
   `,
   */
+  /* mark v1.17
+   template: `
+   <section>
+     <form>
+       <input type="text" placeholder="Filter by city">
+       <button class="primary" type="button">Search</button>
+     </form>
+   </section>
+   <section class="results">
+   <app-housing-location
+   *ngFor="let housingLocation of housingLocationList"
+   [housingLocation]="housingLocation">
+ </app-housing-location>
+   </section>
+   `,
+ */
   template: `
-  <section>
-    <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
-    </form>
-  </section>
-  <section class="results">
-  <app-housing-location
-  *ngFor="let housingLocation of housingLocationList"
-  [housingLocation]="housingLocation">
-</app-housing-location>
-  </section>
-  `,
+<section>
+  <form>
+    <input type="text" placeholder="Filter by city" #filter>
+    <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
+  </form>
+</section>
+<section class="results">
+<app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location>
+</section>
+`,
   styleUrls: ['./home.component.css']
 })
 /* mark v1.10
@@ -91,12 +104,29 @@ export class HomeComponent {
 //add v1.10
 export class HomeComponent {
 
+  //add v1.17
+  filteredLocationList: HousingLocation[] = [];
+
+  //add v1.17
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+
   //add v1.11
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList; //add v1.17
   }
+
+
 
   /* mark v1.11
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
